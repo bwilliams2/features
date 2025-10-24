57ad7c4e72beaf3c58305494b1128c64b2d5c75a
Bryce Williams <bwilliams@qventus.com>
2025-10-24 10:50:00 -0500

    Update mounts

---
+++ src/sshmount/README.md
@@ -0,0 +1,24 @@
+
+# ssh-mount (sshmount)
+
+Mounts the host's ~/.ssh directory into the container
+
+## Example Usage
+
+```json
+"features": {
+    "ghcr.io/bwilliams2/ssh-mount/sshmount:0": {}
+}
+```
+
+## Options
+
+| Options Id | Description | Type | Default Value |
+|-----|-----|-----|-----|
+
+
+
+
+---
+
+_Note: This file was auto-generated from the [devcontainer-feature.json](https://github.com/bwilliams2/ssh-mount/blob/main/src/sshmount/devcontainer-feature.json).  Add additional notes to a `NOTES.md`._
---
+++ src/sshmount/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Installing sshmount"
+
+echo "The effective dev container remoteUser is '$_REMOTE_USER'"
+echo "The effective dev container remoteUser's home directory is '$_REMOTE_USER_HOME'"
+
+echo "The effective dev container containerUser is '$_CONTAINER_USER'"
+echo "The effective dev container containerUser's home directory is '$_CONTAINER_USER_HOME'"
+
+chown -R $_REMOTE_USER:$_REMOTE_USER /mnt/.ssh
+ln -s /mnt/.ssh $_REMOTE_USER_HOME/.ssh
+
+# Mount .aws directory
+chown -R $_REMOTE_USER:$_REMOTE_USER /mnt/.aws
+ln -s /mnt/.aws $_REMOTE_USER_HOME/.aws
+
+# Mount .config/fish directory
+mkdir -p $_REMOTE_USER_HOME/.config
+chown -R $_REMOTE_USER:$_REMOTE_USER /mnt/.config/fish
+ln -s /mnt/.config/fish $_REMOTE_USER_HOME/.config/fish
